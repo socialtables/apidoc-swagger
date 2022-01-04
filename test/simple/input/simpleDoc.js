2eb1aea4093d8ce66c3019fbbe57b0c82d79a9d2
Andrew Csontos <andrew.csontos@socialtables.com>
2022-01-03 19:28:39 -0500

    add support support for headers

--- test/simple/input/simpleDoc.js
+++ test/simple/input/simpleDoc.js
@@ -4,7 +4,7 @@
  * @apiGroup User
  *
  * @apiParam {Number} id Users unique ID.
- *
+ * @apiHeader {String} [accept] Specify image/png to recieve an image response
  * @apiSuccess {String} firstname Firstname of the User.
  * @apiSuccess {String} lastname  Lastname of the User.
  */
